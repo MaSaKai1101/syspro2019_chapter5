fa5d18a3b09de44673b6d919f01e25cbdb72afa3
MaSaKai1652 <shatanaka7@gmail.com>
2019-09-26 11:30:03 +0900

    First_commit

--- temperatur2.py
+++ temperatur2.py
@@ -3,10 +3,46 @@
 
 from smbus2 import SMBus
 import time
-import datetime
+from datetime import date, datetime
 import json
-import conllections as cl
+import collections as cl
 import temperature
+
+i2c_address = 0x76
+bus_number = 1
+bus = SMBus(bus_number)
+
+def json_serial(obj):
+    if isinstance(obj, (datetime, date)):
+        return obj.isoformat()
+
+    raise TypeError ("Type %s not serializable" % type(obj))
+
+def print_json(filename):
+    file = open(filename, 'r')
+    json_data = json.load(file)
+
+    print("{}".format(json.dumps(json_data, indent = 4, default = json_serial)))
+
+
+def write_json(filename):
+	DataName_list = ["time", "temp", "pressure", "hum"]
+	dt_now = datetime.now()
+        value = readData_mk2()
+	value_list = [dt_now, value[0], value[1], value[2]]
+
+	ys = cl.OrderedDict()
+	for g in range(len(DataName_list)):
+       		data = cl.OrderedDict()
+        	data["time"] = value_list[0]
+        	data["temp"] = value_list[1]
+        	data["pressure"] = value_list[2]
+        	data["hum"] = value_list[3]
+
+        	ys[DataName_list[g]] = data
+
+   	file = open(filename, 'w')
+    	json.dump(ys, file, indent = 4, default = json_serial)
 
 def readData_mk2():
         data = []
@@ -24,24 +60,12 @@ def readData_mk2():
 
 
 if __name__ == '__main__':
-	DataName_list = ["time", "temp", "pressure", "hum"]
-	value_list = []
-
-	ys = cl.OrderedDict()
 	i = 0
+	m = 0
 	while 1:
         	time.sleep(1)
-        	i++
-        	if(i == 10):
-        		dt_now = datetime.datetime.now()
-        		print(dt_now)
-			value = readData_mk2()
-			value_list = [dt_now, value[0], value[1], value[2]]
-
-			data = cl.OrderedDict()
-			data["time"] = value_list[0]
-			data["temp"] = value_list[1]
-			data["pressure"] = value_list[2]
-			data["hum"] = value_list[3]
-
-			ys[DataName_list] = data
+        	if(i == m * 10):
+			m = m + 1			
+			write_json("sample.json")
+			print_json("sample.json")
+		i = i + 1
